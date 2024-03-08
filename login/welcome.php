752c463f3f21b9b1d2e3bdee17f2201cf46c85a2
hrushali <hrushalipatil2002@gmail.com>
2024-03-08 20:57:41 +0530

    add project hrush

---
+++ login/welcome.php
@@ -0,0 +1,31 @@
+<?php
+include ("connect.php");
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+    <style>
+        h1{
+            color: green;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+    </style>
+
+</head>
+<body>
+    <h1>WELCOME TO LOGIN PAGE...</h1>
+    <h3><a href="login.php">logout</a></h3>
+</body>
+</html>
+
+
+
+
